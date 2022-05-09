d8a118bb17c3d8f91778bebc7d0ba4abe89fbd4f
zengyou <15989788839@@qq.com>
2022-05-09 14:46:46 +0800

    ...

--- icu-backstage/src/main/java/com/icu/backstage/BackstageApplication.java
+++ icu-backstage/src/main/java/com/icu/backstage/BackstageApplication.java
@@ -1,13 +1,13 @@
-package com.icu.admin;
+package com.icu.backstage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AdminApplication {
+public class BackstageApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AdminApplication.class, args);
+		SpringApplication.run(BackstageApplication.class, args);
 	}
 
 
