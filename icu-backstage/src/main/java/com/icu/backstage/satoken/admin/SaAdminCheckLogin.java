4927e0edc660d6d3a822fd269c0e1a8b551495d1
zengyou <15989788839@qq.com>
2022-05-26 23:03:04 +0800

    ...

---
+++ icu-backstage/src/main/java/com/icu/backstage/satoken/admin/SaAdminCheckLogin.java
@@ -0,0 +1,20 @@
+package com.icu.backstage.satoken.admin;
+
+import cn.dev33.satoken.annotation.SaCheckLogin;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 登录认证(Admin版)：只有登录之后才能进入该方法，可标注在函数、类上（效果等同于标注在此类的所有方法上）
+ *
+ * @author 曾有
+ * @since 2022/5/26
+ */
+@SaCheckLogin(type = StpAdminUtil.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface SaAdminCheckLogin {
+}
