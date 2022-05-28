cdd097c9eaa3dbffac7e044a9849989fc3a55c63
zengyou <15989788839@qq.com>
2022-05-28 08:56:20 +0800

    ..

--- db/public.sql
+++ db/public.sql
@@ -12,7 +12,7 @@
  Target Server Version : 140002
  File Encoding         : 65001
 
- Date: 18/05/2022 18:09:51
+ Date: 28/05/2022 08:56:05
 */
 
 
@@ -50,8 +50,8 @@ CREATE TABLE "public"."admin" (
   "name" varchar(64) COLLATE "pg_catalog"."default" DEFAULT ''::character varying,
   "email" varchar(128) COLLATE "pg_catalog"."default" DEFAULT ''::character varying,
   "department_id" int8 DEFAULT 0,
-  "add_time" timestamp(6),
-  "edit_time" timestamp(6),
+  "add_time" timestamp(0),
+  "edit_time" timestamp(0),
   "add_admin" int8 DEFAULT 0,
   "edit_admin" int8 DEFAULT 0,
   "del" char(1) COLLATE "pg_catalog"."default" DEFAULT '0'::bpchar,
@@ -90,8 +90,8 @@ CREATE TABLE "public"."menu" (
   "icon" varchar(32) COLLATE "pg_catalog"."default" DEFAULT ''::character varying,
   "sort" int2 DEFAULT 0,
   "type" char(1) COLLATE "pg_catalog"."default" DEFAULT '0'::bpchar,
-  "add_time" timestamp(6),
-  "edit_time" timestamp(6),
+  "add_time" timestamp(0),
+  "edit_time" timestamp(0),
   "add_admin" int8 DEFAULT 0,
   "edit_admin" int8 DEFAULT 0,
   "del" char(1) COLLATE "pg_catalog"."default" DEFAULT '0'::bpchar
@@ -114,6 +114,30 @@ COMMENT ON TABLE "public"."menu" IS '菜单';
 -- ----------------------------
 -- Records of menu
 -- ----------------------------
+INSERT INTO "public"."menu" VALUES (1, '权限管理', '', '', 0, '', 0, '0', NULL, NULL, 0, 0, '0');
+INSERT INTO "public"."menu" VALUES (2, '菜单管理', '', 'menu/list', 1, '', 0, '0', NULL, NULL, 0, 0, '0');
+INSERT INTO "public"."menu" VALUES (3, '菜单添加', '', 'menu/add', 2, '', 0, '1', NULL, NULL, 0, 0, '0');
+INSERT INTO "public"."menu" VALUES (4, '', '', '', 0, '', 0, '1', NULL, NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (5, '', '', '', 0, '', 0, '1', NULL, NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (7, '', '', '', 0, '', 0, '1', NULL, NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (8, '', '', '', 0, '', 0, '1', '2022-05-27 00:00:00', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (9, '', '', '', 0, '', 0, '1', '2022-05-27 00:00:00', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (10, '', '', '', 0, '', 0, '1', NULL, NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (6, '', '', '', 0, '', 0, '1', '2022-05-27 00:00:00', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (12, '', '', '', 0, '', 0, '1', NULL, NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (13, '', '', '', 0, '', 0, '1', '2022-05-27 00:00:00', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (14, '', '', '', 0, '', 0, '1', '2022-05-27 00:00:00', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (11, '', '', '', 0, '', 0, '1', '2022-05-27 00:00:00', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (15, '', '', '', 0, '', 0, '1', '2022-05-27 00:00:00', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (16, '', '', '', 0, '', 0, '1', '2022-05-27 17:56:18', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (17, '', '', '', 0, '', 0, '1', '2022-05-27 17:56:36', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (18, '', '', '', 0, '', 0, '1', '2022-05-27 17:56:36', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (19, '', '', '', 0, '', 0, '1', '2022-05-27 17:57:09', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (20, '', '', '', 0, '', 0, '1', '2022-05-27 17:57:38', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (21, '', '', '', 0, '', 0, '1', '2022-05-27 17:58:07', NULL, 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (22, '', '', '', 0, '', 0, '1', '2022-05-27 18:03:42', '2022-05-27 18:03:42', 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (23, '', '', '', 0, '', 0, '1', '2022-05-27 18:05:30', '2022-05-27 18:05:30', 1, 0, '0');
+INSERT INTO "public"."menu" VALUES (24, '', '', '', 0, '', 0, '1', '2022-05-27 18:07:25', '2022-05-27 18:07:25', 1, 0, '0');
 
 -- ----------------------------
 -- Alter sequences owned by
@@ -127,7 +151,7 @@ SELECT setval('"public"."admin_id_seq"', 1, true);
 -- ----------------------------
 ALTER SEQUENCE "public"."menu_id_seq"
 OWNED BY "public"."menu"."id";
-SELECT setval('"public"."menu_id_seq"', 1, false);
+SELECT setval('"public"."menu_id_seq"', 24, true);
 
 -- ----------------------------
 -- Uniques structure for table admin
