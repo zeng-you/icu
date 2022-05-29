0ad7d5d496ef93ce8efa79e5f03bb71afe4138a2
zengyou <15989788839@qq.com>
2022-05-29 21:24:12 +0800

    ...

---
+++ icu-backstage/src/main/java/com/icu/backstage/constant/CacheConstant.java
@@ -0,0 +1,13 @@
+package com.icu.backstage.constant;
+
+/**
+ * 缓存常量
+ * @author 曾有
+ * @since 2022/5/29
+ */
+public interface CacheConstant {
+
+    String MENU_LIST = "";
+    String MENU_     = "";
+
+}
